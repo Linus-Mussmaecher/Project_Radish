fb323704fcf323387d23185ae63ca77cbdd56469
Linus-Mussmaecher <linus.mussmaecher@gmail.com>
2023-04-15 01:14:25 +0200

    Updated sprite drawing to flip when moving left

--- src/game_state/components/sprite.rs
+++ src/game_state/components/sprite.rs
@@ -1,6 +1,6 @@
 use ggez::{
     glam::Vec2,
-    graphics::{Canvas, DrawParam},
+    graphics::{Canvas, DrawParam, Drawable},
     Context,
 };
 use mooeye::sprite;
@@ -15,21 +15,20 @@ pub fn draw_sprites(world: &mut World, ctx: &Context, canvas: &mut Canvas) {
     for (pos, sprite, vel) in
         <(&Position, &mut sprite::Sprite, Option<&Velocity>)>::query().iter_mut(world)
     {
-        let factor = match vel {
-            Some(v) => {
-                if v.get_dx() >= 0. {
-                    1.
-                } else {
-                    -1.
-                }
-            }
-            None => 1.,
+        let (factor, pos) = if match vel {
+            Some(v) => v.get_dx() < 0.,
+            None => false,
+        }{
+            (-1., *pos + Vec2::new(sprite.dimensions(ctx).unwrap_or_default().w * PIXEL_SIZE, 0.))
+        } else {
+            (1., *pos)
         };
+
         sprite.draw_sprite(
             ctx,
             canvas,
             DrawParam::default()
-                .dest(*pos)
+                .dest(pos)
                 .scale(Vec2::new(PIXEL_SIZE * factor, PIXEL_SIZE)),
         );
     }
