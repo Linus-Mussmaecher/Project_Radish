a8491e2e0f44f15f7ddaefa51bb52b25870ba52b
Linus-Mussmaecher <linus.mussmaecher@gmail.com>
2023-05-02 01:42:08 +0200

    Added highscore tracking

--- src/scenes/in_game_menu.rs
+++ src/scenes/in_game_menu.rs
@@ -2,7 +2,8 @@ use ggez::{
     graphics::{self, Color, TextFragment},
     GameError,
 };
-use mooeye::{*};
+use mooeye::*;
+use serde::{Deserialize, Serialize};
 
 use crate::PALETTE;
 
@@ -113,10 +114,7 @@ impl scene_manager::Scene for InGameMenu {
                 .keyboard
                 .is_key_just_pressed(ggez::winit::event::VirtualKeyCode::M)
         {
-            res = scene_manager::SceneSwitch::replace(
-                super::main_menu::MainMenu::new(ctx)?,
-                2,
-            );
+            res = scene_manager::SceneSwitch::replace(super::main_menu::MainMenu::new(ctx)?, 2);
         }
 
         if messages.contains(&UiMessage::Clicked(3))
@@ -141,6 +139,15 @@ impl scene_manager::Scene for InGameMenu {
     }
 }
 
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+struct Score {
+    score: i32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct ScoreList {
+    scores: Vec<Score>,
+}
 
 pub struct GameOverMenu {
     ui: UiElement<()>,
@@ -148,6 +155,50 @@ pub struct GameOverMenu {
 
 impl GameOverMenu {
     pub fn new(score: i32, ctx: &ggez::Context) -> Result<Self, GameError> {
+        // load highscores
+
+        let mut highscores: Vec<Score> =
+            if let Ok(file) = std::fs::read_to_string("./data/highscores.toml") {
+                toml::from_str::<ScoreList>(&file)
+                    .map(|sl| sl.scores)
+                    .unwrap_or_else(|_| Vec::new())
+            } else {
+                Vec::new()
+            };
+
+        // if only a small amount of scores is recorded or the worst result from the list was beaten, insert the new result
+        let own_index = if highscores.len() < 25
+            || score >= highscores.last().map(|a| a.score).unwrap_or_default()
+        {
+            // set default index: at the end of the list
+            let mut index = highscores.len();
+            // see if there is an appropriate earlier index
+            for (i, value) in highscores.iter().enumerate() {
+                if score >= value.score {
+                    index = i;
+                    break;
+                }
+            }
+            // insert at appropriate index
+            highscores.insert(index, Score { score });
+            // if list has grown too much, cut last element
+            if highscores.len() > 25 {
+                highscores.pop();
+            }
+            // return selected index
+            Some(index)
+        } else {
+            None
+        };
+
+        // save highscores
+        if let Ok(toml_string) = toml::to_string(&ScoreList {
+            scores: highscores.clone(),
+        }) {
+            std::fs::write("./data/highscores.toml", &toml_string)?;
+        }
+
+        // create UI
         let box_vis = ui_element::Visuals {
             background: Color::from_rgb_u32(PALETTE[0]),
             border: Color::from_rgb_u32(PALETTE[7]),
@@ -174,15 +225,54 @@ impl GameOverMenu {
         .build();
         main_box.add(game_over)?;
 
-        let score = ggez::graphics::Text::new(
-            graphics::TextFragment::new(format!("Score: {}", score)).color(Color::from_rgb_u32(PALETTE[6])),
+        let mut score_box = containers::HorizontalBox::new();
+        score_box.spacing = 35.;
+
+        let score_disp = ggez::graphics::Text::new(
+            graphics::TextFragment::new("Your score:\n")
+                .color(Color::from_rgb_u32(PALETTE[7]))
+                .scale(36.),
+        )
+        .add(
+            graphics::TextFragment::new(format!("  {:>5}", score))
+                .color(Color::from_rgb_u32(PALETTE[6]))
+                .scale(32.),
         )
         .set_font("Retro")
-        .set_scale(32.)
         .to_owned()
         .to_element_builder(0, ctx)
+        .with_alignment(ui_element::Alignment::Center, ui_element::Alignment::Min)
         .build();
-        main_box.add(score)?;
+        score_box.add(score_disp)?;
+
+        let mut highscore_disp = ggez::graphics::Text::new(
+            graphics::TextFragment::new("Highscores:\n")
+                .color(Color::from_rgb_u32(PALETTE[7]))
+                .scale(36.),
+        );
+
+        for (index, value) in highscores.iter().enumerate().take(5) {
+            highscore_disp.add(
+                graphics::TextFragment::new(format!("  {:02}.{:>5}\n", index + 1, value.score))
+                    .color(Color::from_rgb_u32(if index == own_index.unwrap_or(128) {
+                        PALETTE[8]
+                    } else {
+                        PALETTE[6]
+                    }))
+                    .scale(32.),
+            );
+        }
+
+        let highscore_disp = highscore_disp
+            .set_font("Retro")
+            .to_owned()
+            .to_element_builder(0, ctx)
+            .with_alignment(ui_element::Alignment::Center, ui_element::Alignment::Min)
+            .build();
+
+        score_box.add(highscore_disp)?;
+
+        main_box.add(score_box.to_element(0, ctx))?;
 
         let restart = ggez::graphics::Text::new(
             graphics::TextFragment::new("Restart").color(Color::from_rgb_u32(PALETTE[6])),
@@ -197,7 +287,8 @@ impl GameOverMenu {
         main_box.add(restart)?;
 
         let main_menu = ggez::graphics::Text::new(
-            graphics::TextFragment::new("Return to Main Menu").color(Color::from_rgb_u32(PALETTE[6])),
+            graphics::TextFragment::new("Return to Main Menu")
+                .color(Color::from_rgb_u32(PALETTE[6])),
         )
         .set_font("Retro")
         .set_scale(32.)
@@ -208,15 +299,14 @@ impl GameOverMenu {
         .build();
         main_box.add(main_menu)?;
 
-        let main_box = main_box.to_element_builder(0, ctx)
-        .with_visuals(box_vis)
-        .with_alignment(ui_element::Alignment::Center, ui_element::Alignment::Center)
-        .with_padding((25., 25., 25., 25.))
-        .build();
+        let main_box = main_box
+            .to_element_builder(0, ctx)
+            .with_visuals(box_vis)
+            .with_alignment(ui_element::Alignment::Center, ui_element::Alignment::Center)
+            .with_padding((25., 25., 25., 25.))
+            .build();
 
-        Ok(Self {
-            ui: main_box,
-        })
+        Ok(Self { ui: main_box })
     }
 }
 
@@ -240,7 +330,10 @@ impl scene_manager::Scene for GameOverMenu {
                 .keyboard
                 .is_key_just_pressed(ggez::winit::event::VirtualKeyCode::M)
         {
-            return Ok(mooeye::scene_manager::SceneSwitch::replace(super::main_menu::MainMenu::new(ctx)? , 2));
+            return Ok(mooeye::scene_manager::SceneSwitch::replace(
+                super::main_menu::MainMenu::new(ctx)?,
+                2,
+            ));
         }
         Ok(mooeye::scene_manager::SceneSwitch::None)
     }
